8ae647b440d4633bf393113f5a7a8db3e7a3fff2
Ernesto Anaya <ernesto.anaya@bbva.com>
2023-08-06 01:21:38 -0500

    fix https on DO x2

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
                 'resources/js/app.js',
             ],
             refresh: true,
+            server: { https: true },
         }),
     ],
     server: { https: true },
